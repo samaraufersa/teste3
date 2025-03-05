d655202ebfe1d67ef9f51e307147abd480b0e5d1
samaraufersa <109767273+samaraufersa@users.noreply.github.com>
2025-03-05 11:16:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.title('Localização das comunidades quilombolas (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')

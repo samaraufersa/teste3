3e73657b083e73a85a7c746a70c7f5f474537822
samaraufersa <109767273+samaraufersa@users.noreply.github.com>
2025-03-05 11:19:49 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -30,7 +30,7 @@ fig_qnt_estado = px.bar(df_qnt_estado, x="NM_UF", y="total", color="total", titl
 fig_qnt_estado.update_layout(xaxis_title= "Estado", yaxis_title= "Quantidade")
 st.plotly_chart(fig_qnt_estado, use_container_width=True)
 
-df_qnt_mun = df['NM_MUNIC'].value_counts().reset_index(name="total")
+df_qnt_mun = df['NM_MUNIC'].value_counts()[:10].reset_index(name="total")
 fig_qnt_mun = px.bar(df_qnt_mun, x="NM_MUNIC", y="total", color="total", title="Quantidade de Pessoas por Municipio")
 fig_qnt_mun.update_layout(xaxis_title= "Municipio", yaxis_title= "Quantidade")
 st.plotly_chart(fig_qnt_mun, use_container_width=True)
